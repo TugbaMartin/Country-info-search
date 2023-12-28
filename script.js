6ee11511642ad7d1ac6e8e3161f38e99493e98ee
TugbaMartin <121744112+TugbaMartin@users.noreply.github.com>
2023-12-28 18:30:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const currency = document.getElementById("currency");
 const currencyShort = document.getElementById("CurrencyShort");
 const language = document.getElementById("language");
 const searchResult = document.getElementById("search_result");
-
-search.addEventListener("click", () => {
+searchForm.addEventListener("submit", (event) => { // Update to listen for form submission
+    event.preventDefault();
     let countryName = countryInput.value;
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
 
@@ -28,8 +28,15 @@ search.addEventListener("click", () => {
             currency.innerHTML = countryData.currencies[Object.keys(countryData.currencies)[0]].name;
             currencyShort.innerHTML = Object.keys(countryData.currencies)[0];
             language.innerHTML = Object.values(countryData.languages).toString();
+            countryInput.value = '';
+            document.getElementById("error").style.display = 'none'; // Hide error message if displayed
+       
         })
+
+
         .catch((error) => {
             console.error('Error fetching data:', error);
+            document.getElementById("error").innerText = 'Error fetching data. Please try again.'; // Show error message
+            document.getElementById("error").style.display = 'block'; // Display error message
         });
 });
