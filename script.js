25737a7bb6beaacf5c61c854855918c47be6f3fe
TugbaMartin <121744112+TugbaMartin@users.noreply.github.com>
2023-12-29 13:29:54 +0300

    Most Updated script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countryInput = document.getElementById("countryInput");
-const search = document.getElementById("search");
+const searchForm = document.getElementById("searchForm");
 const flag = document.getElementById("flag");
 const name = document.getElementById("name");
 const capital = document.getElementById("capital");
@@ -9,14 +9,25 @@ const currency = document.getElementById("currency");
 const currencyShort = document.getElementById("CurrencyShort");
 const language = document.getElementById("language");
 const searchResult = document.getElementById("search_result");
-searchForm.addEventListener("submit", (event) => { // Update to listen for form submission
+const errorMessage = document.getElementById("error");
+
+searchForm.addEventListener("submit", (event) => {
     event.preventDefault();
     let countryName = countryInput.value;
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
 
     fetch(finalURL)
-        .then((response) => response.json())
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+            return response.json();
+        })
         .then((data) => {
+            if (data.length === 0) {
+                throw new Error('No data found for the specified country.');
+            }
+
             searchResult.style.display = 'block';
             const countryData = data[0];
             console.log(countryData);
@@ -29,14 +40,28 @@ searchForm.addEventListener("submit", (event) => { // Update to listen for form 
             currencyShort.innerHTML = Object.keys(countryData.currencies)[0];
             language.innerHTML = Object.values(countryData.languages).toString();
             countryInput.value = '';
-            document.getElementById("error").style.display = 'none'; // Hide error message if displayed
-       
+            errorMessage.style.display = 'none'; // Hide error message if displayed
         })
-
-
         .catch((error) => {
             console.error('Error fetching data:', error);
-            document.getElementById("error").innerText = 'Error fetching data. Please try again.'; // Show error message
-            document.getElementById("error").style.display = 'block'; // Display error message
+            if (error.message === 'No data found for the specified country.') {
+                errorMessage.innerText = error.message;
+            } else {
+                errorMessage.innerText = 'Error fetching data. Please try again.';
+            }
+            errorMessage.style.display = 'block'; // Display error message
+            clearSearchResult();
         });
 });
+
+// Function to clear search result fields
+function clearSearchResult() {
+    flag.src = '';
+    name.innerHTML = '';
+    capital.innerHTML = '';
+    continent.innerHTML = '';
+    population.innerHTML = '';
+    currency.innerHTML = '';
+    currencyShort.innerHTML = '';
+    language.innerHTML = '';
+}
